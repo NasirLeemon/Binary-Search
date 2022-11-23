20c7f68ba03992bf1259adb94236ab322a774592
Abu Nasir Leemon <leemon174@gmail.com>
2022-11-23 20:32:43 +0600

    problem solved

--- floorAndCeil.js
+++ floorAndCeil.js
@@ -1,7 +1,4 @@
-
-
 // Flooring and Ceiling
-
 
 function floorAndCeil(arr, target) {
   let start = 0;
@@ -9,32 +6,36 @@ function floorAndCeil(arr, target) {
   let mid;
   let asc;
   if (start < end) {
-    asc = true
+    asc = true;
   } else {
-    asc = false
+    asc = false;
   }
 
-      while(start <= end){
-        mid = Math.floor( (start + end) / 2)
-      if (asc) {
-        if (arr[mid] > target) {
-            end = mid - 1
-        } else {
-            start = mid + 1
-        }
+  while (start <= end) {
+    mid = Math.floor((start + end) / 2);
+    if (asc) {
+      if (arr[mid] > target) {
+        end = mid - 1;
       } else {
-        if ( arr[mid] > target ) {
-            start = mid + 1
-        } else {
-            end = mid -1
-        }
+        start = mid + 1;
       }
-
+    } else {
+      if (arr[mid] > target) {
+        start = mid + 1;
+      } else {
+        end = mid - 1;
+      }
     }
- return 'Floor is ' + arr[mid - 1] + ' and Ceil is ' + arr[mid]
+  }
+  if (target < arr[start + 1]) {
+    return "Floor is " + arr[start - 1] + " and Ceil is " + arr[start];
+  }
 
+  return "Floor is " + arr[mid - 1] + " and Ceil is " + arr[mid];
 }
 
 console.log(floorAndCeil([10, 20, 30, 40, 50, 60, 70, 80, 90, 100], 57));
+console.log(floorAndCeil([10, 20, 30, 40, 50, 60, 70, 80, 90, 100], 11));
+
 
 // Problem = if we take 10-19 as target than it will show undefined!
